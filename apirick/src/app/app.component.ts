80954f6700691408c44bbd82d8855e7ded4583d0
/Luis R Collymoore <lcollymoore@gmail.com>
2023-11-12 20:25:31 -0400

    first-commit

---
+++ apirick/src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  characters: any;
+  counter = 0;
+  ngOnInit() {
+    this.loadPage();
+  }
+
+  loadPage(page: number = this.counter) {
+    // Realizamos una solicitud GET a la página especificada
+    this.http
+      .get('https://rickandmortyapi.com/api/character?page=' + page)
+      .subscribe(
+        (response: any) => {
+          // Actualizamos la lista de personajes con la respuesta de la API
+          this.characters = response;
+          console.log(response);
+        },
+        (error: any) => {
+          // Manejamos el error
+        }
+      );
+
+    this.counter++;
+    console.log(this.counter);
+  }
+}
