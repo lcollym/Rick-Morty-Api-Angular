221cfeb4114dc39ff8becb820942b7c106d11738
/Luis R Collymoore <lcollymoore@gmail.com>
2023-11-16 14:46:39 -0400

    ultimate commit

--- apirick/src/app/app.component.ts
+++ apirick/src/app/app.component.ts
@@ -3,27 +3,45 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HttpClientModule],
+  imports: [CommonModule,HttpClientModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   constructor(private http: HttpClient) {}
-
+  isHidden = true;
   characters: any;
-  maxchar = 0;
-  minchar = 0;
+  pages = 2;
+
   ngOnInit() {
     this.loadPage();
   }
 
-  loadPage(page: number = this.maxchar) {
+ async nextPage(page: number = this.pages) {
     // Realizamos una solicitud GET a la página especificada
-    this.http
+   await this.http
+      .get('https://rickandmortyapi.com/api/character?page=' + page)
+      .subscribe(
+        (response: any) => {
+          // Actualizamos la lista de personajes con la respuesta de la API
+          this.characters = response;
+          console.log(response);
+        },
+        (error: any) => {
+          // Manejamos el error
+        }
+      );
+      this.increment();
+
+  }
+  async previusPage(page: number = this.pages) {
+    // Realizamos una solicitud GET a la página especificada
+   await this.http
       .get('https://rickandmortyapi.com/api/character?page=' + page)
       .subscribe(
         (response: any) => {
@@ -36,7 +54,50 @@ export class AppComponent implements OnInit {
         }
       );
 
-    this.maxchar++;
-    console.log(this.maxchar);
+
+      
+   
+
+    this.decrement()
+
+  }
+  loadPage() {
+    // Realizamos una solicitud GET a la página especificada
+    this.http
+      .get('https://rickandmortyapi.com/api/character')
+      .subscribe(
+        (response: any) => {
+          // Actualizamos la lista de personajes con la respuesta de la API
+          this.characters = response;
+          console.log(response);
+        },
+        (error: any) => {
+          // Manejamos el error
+        }
+      );
+
+
+     
+  
+  }
+
+
+  increment(){
+    this.pages++;
+    console.log(this.pages);
+    if(this.pages >= 43){
+      alert('There are no more characters :(')
+      location.reload()
+    }
+    this.isHidden = false;
+  }
+
+  decrement(){
+    this.pages--;
+    console.log(this.pages);
+    if(this.pages <= 0){
+      this.isHidden = true;
+     
+    }
   }
 }
--- apirick/src/app/app.routes.ts
+++ apirick/src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import {  Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+
+      {
+        path: '',
+        component:AboutComponent
+      }
+    ];
+
